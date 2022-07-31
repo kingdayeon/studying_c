b32ec38e6cd12df9a27b48f63331f4c79f943c3b
kingdayeon <105100662+kingdayeon@users.noreply.github.com>
2022-07-31 22:31:02 +0900

    Create 3-4_ASC.c

---
+++ 3-4_ASC.c
@@ -0,0 +1,13 @@
+#include <stdio.h>//Include a ‘stdio.h’ file to my code 
+
+int main(void) {//create a main function
+
+  char ASC; //declaring
+  
+  printf("input an alphabet: ");//printf function
+  scanf("%c",&ASC);//store the input value of alphabet
+  printf("%d",ASC); //print ASCII on the screen 
+  
+  return 0; //way to exit the function
+
+}
