d6a5361ad7da83dabb9590973088c89144a19310
kingdayeon <105100662+kingdayeon@users.noreply.github.com>
2022-07-31 22:53:18 +0900

    Create 9-1_dynamicLargest.c

---
+++ 9-1_dynamicLargest.c
@@ -0,0 +1,29 @@
+//Q1
+#include <stdio.h> //Include a ‘stdio.h’ file to my code
+#include <stdlib.h>//Include a ‘stdlib.h’ file to my code
+
+int main(void) { //create a main function
+  int num, n; //declare a variable of integer
+  float * dynamic = (float*)malloc (sizeof(float) * n); //using malloc
+  float max=0; //declare a variable of float
+  
+  printf("Enter the total number of elements: "); //printf function
+  scanf("%d",&num); //store the input value of integer
+  printf("Enter Number 1: ");//printf function
+  scanf("%f", &dynamic[0]); //store the input value
+  printf("Enter Number 2: ");
+  scanf("%f", &dynamic[1]);
+  printf("Enter Number 3: ");
+  scanf("%f", &dynamic[2]);
+
+  for(int i=0 ; i<num ; i++){ //finding largest number
+    if(dynamic[i]>max) 
+      max = dynamic[i];
+  }
+
+  printf("Largest number = %.2f", max);//printf function
+
+  free(dynamic); //deallocate the allocated memory
+  
+  return 0; //way to exit the function
+}
