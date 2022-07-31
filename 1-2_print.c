c1dc8b940b9f89d80f17583b2e9d1df97b8b2be1
kingdayeon <105100662+kingdayeon@users.noreply.github.com>
2022-07-31 22:24:31 +0900

    Create 1-2_print.c

---
+++ 1-2_print.c
@@ -0,0 +1,7 @@
+#include<stdio.h> //Include a ‘stdio.h’ file to my code 
+
+int main(){ //create a main function
+   printf("1\n2\n3\n4\n5\n"); //print on the screen, using \n which makes next line printing
+   
+   return 0; //Way to exit the function
+}
