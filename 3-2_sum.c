4164625257fbb5844b036752169cfe3985a86ccd
kingdayeon <105100662+kingdayeon@users.noreply.github.com>
2022-07-31 22:30:24 +0900

    Create 3-2_sum.c

---
+++ 3-2_sum.c
@@ -0,0 +1,14 @@
+#include <stdio.h>//Include a ‘stdio.h’ file to my code 
+
+int main(void) {//create a main function
+
+  int apples = 1800, oranges = 450, bananas = 8500; //declare and initialize variables
+  int sum; //declare a variable
+  
+  sum = apples + oranges + bananas; //initialize a variable
+  
+  printf("total = %d won",sum); //printf function
+  
+  return 0; //way to exit the function
+
+}
